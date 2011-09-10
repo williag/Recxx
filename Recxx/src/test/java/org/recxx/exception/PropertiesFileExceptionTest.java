e68f6c0ce3da5d998c58b13c692e5591bfcb4891
Graham Williamson <grahamwilliamson@Graham-Williamsons-MacBook-Pro.local>
2011-09-10 11:57:55 +0100

    Tested PropertiesFileException
    Added tests for the PropertiesFileException constructor to check that
    the message and cause are set. Used hamcrest and therefore added
    dependency on hamcrest to pom.

--- Recxx/src/test/java/org/recxx/exception/PropertiesFileExceptionTest.java
+++ Recxx/src/test/java/org/recxx/exception/PropertiesFileExceptionTest.java
@@ -1,11 +1,25 @@
 package org.recxx.exception;
 
-/**
- * Created by Shaine Ismail.
- * User: sismail
- * Date: 23/08/2011
- * Time: 07:10
- * Copyright SNI-Services ltd
- */
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 public class PropertiesFileExceptionTest {
+
+	@Test
+	public void constructorShouldSetMessage() throws Exception {
+		String inputMessage = "Test Message";
+		PropertiesFileException exception = new PropertiesFileException(inputMessage);
+		assertThat(exception.getMessage(), is(inputMessage));
+	}
+
+	@Test
+	public void constructorShouldSetMessageAndCause() throws Exception {
+		String inputMessage = "Test Message";
+		Throwable inputCause = new Exception("Test Cause");
+		PropertiesFileException exception = new PropertiesFileException(inputMessage, inputCause);
+		assertThat(exception.getMessage(), is(inputMessage));
+		assertThat(exception.getCause(), is(inputCause));
+	}
 }
