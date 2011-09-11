25deb17aba1fdbc0a95d86a1be814af18567fb59
Graham Williamson <grahamwilliamson@Graham-Williamsons-MacBook-Pro.local>
2011-09-11 16:16:01 +0100

    Made StringUtils constructor private.

--- Recxx/src/main/java/org/recxx/utils/StringUtils.java
+++ Recxx/src/main/java/org/recxx/utils/StringUtils.java
@@ -2,6 +2,9 @@ package org.recxx.utils;
 
 public class StringUtils {
 
+	private StringUtils() {
+	}
+
 	public static boolean isNullOrEmpty(String string) {
 		return string == null || string.equals("");
 	}
